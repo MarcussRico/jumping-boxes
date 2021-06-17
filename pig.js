8a20d7fe3668d39bcf13f25cb6909769022fac89
MarcussRico <74846493+MarcussRico@users.noreply.github.com>
2021-06-17 18:09:58 +0530

    Add files via upload

---
+++ pig.js
@@ -0,0 +1,31 @@
+class Pig{
+    constructor(x,y){
+        var options ={
+        'density' : 1,
+        'restitution': 1,
+        'friction': 1
+        }
+        this.Body = Bodies.rectangle(x,y,50,50,options);
+        this.width = 50;
+        this.height = 50;
+        World.add(world,this.Body)
+    }
+    display(){
+     var pos = this.Body.position;
+    
+     var angle = this.Body.angle; 
+     push()
+     
+     fill('green')
+     translate(pos.x, pos.y)
+     rotate(angle);
+     strokeWeight(3)
+     stroke('yellow')
+     rectMode(CENTER)
+     rect(0, 0, this.width, this.height);
+     pop()
+    
+    }
+    
+    
+    }
